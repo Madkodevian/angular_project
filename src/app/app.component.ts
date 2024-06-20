cb300771bfc29465fcd2fc2a11c38210325fa284
Tatiana Rosales Hernández <madkodevian@gmail.com>
2024-06-20 15:36:12 +0100

    edit commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,12 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from './header/header.component';
 import { RouterOutlet } from '@angular/router';
-import { AvatarComponent } from './avatar/avatar.component';
 import { BtnComponent } from './btn/btn.component';
 import { IconComponent } from './icon/icon.component';
-import { LinkComponent } from './link/link.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, AvatarComponent, BtnComponent, IconComponent, LinkComponent], // Importa los componentes independientes aquí
+  imports: [RouterOutlet, BtnComponent, IconComponent], // Importa los componentes independientes aquí
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'], // Corrige el nombre de la propiedad
 })
